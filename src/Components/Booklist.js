0a0cd058662eeab2ee1ce46220c2cb64dd965aa7
gopalbbharadva <gopalbbharadva@gmail.com>
2021-08-08 12:40:59 +0530

    project setup for booklist

---
+++ src/Components/Booklist.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Booklist extends React.Component{
+    render(){
+        return(
+            <h1>Booklist component</h1>
+        )
+    }
+}
+export default Booklist;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <h1>Navber component</h1>
+        )
+    }
+}
+export default Navbar;
